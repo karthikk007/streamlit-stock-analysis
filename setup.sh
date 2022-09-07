71f7b894387649d26193d549b287b8d6d733cab7
KARTHIK KUMAR <kk007@uber.com>
2022-09-07 14:44:17 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,20 @@
-python3 -m venv env
+ROOT=${WORKSPACE:-$(git rev-parse --show-toplevel)}
+DIR="$ROOT"
+DIR_VENV="$DIR/venv"
+PYTHON="/usr/local/bin/python3"
 
-source env/bin/activate
+
+function setup_venv() {
+    export PIP_CONFIG_FILE=$HOME/.config/pip/pip.conf
+    echo "$PIP_CONFIG_FILE"
+
+    "$PYTHON" -m venv "$DIR_VENV"
+    source "$DIR_VENV/bin/activate"
+  
+    echo "=====checking active virtual environment..."
+    echo "$VIRTUAL_ENV"
+
+    pip install -r "$DIR/requirements.txt"
+}
+
+setup_venv
